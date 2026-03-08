e8adb67a8963f883621cb5c1a05666ab6534f7a0
Quang-To <quangto0502@gmail.com>
2026-03-08 14:26:08 +0700

    Finish Loader

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -6,7 +6,7 @@ def data_loader(filename: str = "data_TSI_v2.csv") -> pd.DataFrame:
 
     project_root = Path(__file__).resolve().parents[2]
 
-    file_location = project_root / "data" / filename
+    file_location = project_root / "data" / "raw_data" / filename
 
     df = pd.read_csv(file_location)
 
