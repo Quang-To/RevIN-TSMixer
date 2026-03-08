784b30cbd34523f09b54ad18f5eed78dae7eb8e8
Quang-To <quangto0502@gmail.com>
2026-03-08 14:13:50 +0700

    Load Data

---
+++ src/data/data_loader.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from pathlib import Path
+
+
+def data_loader(filename: str = "data_TSI_v2.csv") -> pd.DataFrame:
+
+    project_root = Path(__file__).resolve().parents[2]
+
+    file_location = project_root / "data" / filename
+
+    df = pd.read_csv(file_location)
+
+    return df
