e5b591e6ea51295edfb61ee50e9838760f4af7f8
Quang-To <quangto0502@gmail.com>
2026-03-10 03:12:51 +0700

    Loader

---
+++ src/data/dataset.py
@@ -0,0 +1,58 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from src.data.preprocessing import Preprocessing
+
+
+class TimeSeriesData(Dataset):
+
+    def __init__(
+        self,
+        seq_length: int,
+        batch_size: int,
+        pred_length: int = 3,
+        split: str = "train"
+    ):
+
+        self.seq_length = seq_length
+        self.pred_length = pred_length
+        self.batch_size = batch_size
+
+        train, val, test = Preprocessing().preprocess()
+
+        if split == "train":
+            data = train
+            shuffle = True
+        elif split == "val":
+            data = val
+            shuffle = False
+        elif split == "test":
+            data = test
+            shuffle = False
+        else:
+            raise ValueError("split must be train/val/test")
+
+        self.data = torch.tensor(data.values, dtype=torch.float32)
+
+        self.loader = DataLoader(
+            self,
+            batch_size=self.batch_size,
+            shuffle=shuffle
+        )
+
+    def __len__(self):
+        return len(self.data) - self.seq_length - self.pred_length + 1
+
+    def __getitem__(self, idx):
+
+        x = self.data[idx : idx + self.seq_length]
+
+        y = self.data[
+            idx + self.seq_length :
+            idx + self.seq_length + self.pred_length,
+            -1
+        ] 
+
+        return x, y
+
+    def get_loader(self):
+        return self.loader
