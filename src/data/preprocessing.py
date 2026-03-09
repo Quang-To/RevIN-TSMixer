ea44f535d718e998d97498777e84425a9506ba22
Quang-To <quangto0502@gmail.com>
2026-03-10 00:58:09 +0700

    add models

---
+++ src/data/preprocessing.py
@@ -0,0 +1,5 @@
+from .data_loader import data_loader
+
+def preprocessing():
+    data = data_loader()
+    
---
+++ src/models/Mixing Layer/Time-Mixing Layer.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+
+class TimeMixingLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    
